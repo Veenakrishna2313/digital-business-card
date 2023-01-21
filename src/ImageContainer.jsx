b4cacb6c2933a58e7d19004db0307db94756b93b
Veena Krishna Sandana <43022790+Veenakrishna2313@users.noreply.github.com>
2023-01-20 21:19:54 -0800

    Image Container completed

---
+++ src/ImageContainer.jsx
@@ -0,0 +1,11 @@
+function ImageContainer(){
+  return (
+    <div className="image-container">
+      <div className="image"></div>
+      <h1>Veena Krishna</h1>
+      <h2>Front End Developer</h2>
+    </div>
+  );
+
+}
+export default ImageContainer;
